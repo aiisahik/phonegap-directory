a00f222f7599f1cfba50bbccd0dd74007576cbe0
aiisahik <jie.z.zhou@gmail.com>
2013-11-17 20:18:02 -0500

    update

--- js/app.js
+++ js/app.js
@@ -1,18 +1,14 @@
 require.config({
-
     baseUrl: 'js/lib',
-
     paths: {
         app: '../app',
         tpl: '../tpl'
     },
-
     map: {
         '*': {
             'app/models/employee': 'app/models/memory/employee'
         }
     },
-
     shim: {
         'backbone': {
             deps: ['underscore', 'jquery'],
@@ -25,13 +21,10 @@ require.config({
 });
 
 require(['jquery', 'backbone', 'app/router'], function ($, Backbone, Router) {
-
     var router = new Router();
-
     $("body").on("click", ".back-button", function (event) {
         event.preventDefault();
         window.history.back();
     });
-
     Backbone.history.start();
 });
